ee7edd685a033b63bbacef0b451c1630e479d7ec
wheazzyfinesse <59867253+wheazzyfinesse@users.noreply.github.com>
2024-08-13 12:32:00 +0200

    Chat app

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(join(__dirname, "public")));
+
+server.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
+
+let connectedSockets = [];
+
+// Socket.io event handlers
+const onConnected = (socket) => {
+	connectedSockets.push(socket.id);
+
+	io.emit("clients-total", connectedSockets.size);
+
+	socket.on("disconnect", () => {
+		connectedSockets.filter(() => socket.id);
+		io.emit("clients-total", connectedSockets.size);
+	});
+	// Retrieve user messages
+	socket.on("user-message", (data) => {
+		socket.broadcast.emit("chat-message", data);
+	});
+	// Retrieve feedback messages
+	socket.on("feedback-message", (data) => {
+		socket.broadcast.emit("feedback-message", data);
+	});
+};
+
+io.on("connection", onConnected);
---
+++ public/main.js
@@ -0,0 +1,102 @@
+const socket = io();
+const clienstTotal = document.getElementById("clients-total");
+const messageContainer = document.getElementById("message-container");
+const nameInput = document.getElementById("name-input");
+const messageForm = document.getElementById("message-form");
+const messageInput = document.getElementById("message-input");
+
+// Get message from messageInput
+const sendMessage = () => {
+    if (messageInput.value === "") {
+        messageInput.style.border = "1px solid #fff000";
+        return;
+    }
+    messageInput.style.border = "none";
+
+    // Getting the messsage from UI
+    const data = {
+        name: nameInput.value,
+        message: messageInput.value,
+        dateTime: new Date(),
+    };
+    // Send message to socket server
+    socket.emit("user-message", data);
+    addMessageToUI(true, data);
+
+    // Clear messageInput
+    messageInput.value = "";
+};
+
+// Submit form message to socket server
+messageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendMessage();
+});
+
+// Add Message to UI
+const addMessageToUI = (isOwnMessage, data) => {
+    clearFeedbackMessage()
+    const messageElement = `
+                    <li class="${isOwnMessage ? "message-right" : "message-left"
+        }">
+                <p class="message">
+                    ${data.message}
+                    <span>${isOwnMessage ? "me" : data.name} ⚪ ${moment(
+            data.dateTime,
+        ).fromNow()}</span>
+                </p>
+            </li>
+    `;
+    messageContainer.innerHTML += messageElement;
+    scrollToBottom();
+};
+
+// Scroll to bottom of message container
+const scrollToBottom = () => {
+    messageContainer.scrollTo(0, messageContainer.scrollHeight);
+};
+
+messageInput.addEventListener("focus", (e) => {
+    socket.emit("feedback-message", {
+        feedback: `✍🏿 ${nameInput.value} is typing`,
+    });
+});
+messageInput.addEventListener("focus", (e) => {
+    socket.emit("feedback-message", {
+        feedback: `✍🏿 ${nameInput.value} is typing`,
+    });
+});
+messageInput.addEventListener("blur", (e) => {
+    socket.emit("feedback-message", {
+        feedback: "",
+    });
+});
+
+// Clear feedback message
+const clearFeedbackMessage = () => {
+    document.querySelectorAll("li.message-feedback").forEach((element) => {
+        element.parentNode.removeChild(element);
+    })
+}
+
+// Socket Events Handlers================================================================
+// Retrieve number of connected users from socket server
+socket.on("clients-total", (data) => {
+    clienstTotal.innerHTML = `Total connected: ${data}`;
+});
+
+// Retrieve chat messages sent from socket server
+socket.on("chat-message", (data) => {
+    addMessageToUI(false, data);
+});
+// Retrieve feedback messages sent from socket server
+socket.on("feedback-message", (data) => {
+    clearFeedbackMessage()
+    const element = `
+    <li class="message-feedback">
+         <p class="feedback" id="feedback">${data.feedback}</p>
+    </li>`;
+
+    messageContainer.innerHTML += element;
+});
+
