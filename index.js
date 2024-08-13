6410341469d29d640e1a66034b76723c507eda7b
wheazzyfinesse <59867253+wheazzyfinesse@users.noreply.github.com>
2024-08-13 16:03:36 +0200

    Chat app

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import cors from 'cors'
 
 const app = express();
 const server = createServer(app);
@@ -12,31 +13,31 @@ const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.use(express.static(join(__dirname, "public")));
-
+app.use(cors())
 server.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
 let connectedSockets = [];
 
 // Socket.io event handlers
 const onConnected = (socket) => {
-	connectedSockets.push(socket.id);
+    connectedSockets.push(socket.id);
 
-	io.emit("clients-total", connectedSockets.size);
+    io.emit("clients-total", connectedSockets.size);
 
-	socket.on("disconnect", () => {
-		connectedSockets.filter(() => socket.id);
-		io.emit("clients-total", connectedSockets.size);
-	});
-	// Retrieve user messages
-	socket.on("user-message", (data) => {
-		socket.broadcast.emit("chat-message", data);
-	});
-	// Retrieve feedback messages
-	socket.on("feedback-message", (data) => {
-		socket.broadcast.emit("feedback-message", data);
-	});
+    socket.on("disconnect", () => {
+        connectedSockets.filter(() => socket.id);
+        io.emit("clients-total", connectedSockets.size);
+    });
+    // Retrieve user messages
+    socket.on("user-message", (data) => {
+        socket.broadcast.emit("chat-message", data);
+    });
+    // Retrieve feedback messages
+    socket.on("feedback-message", (data) => {
+        socket.broadcast.emit("feedback-message", data);
+    });
 };
 
 io.on("connection", onConnected);
